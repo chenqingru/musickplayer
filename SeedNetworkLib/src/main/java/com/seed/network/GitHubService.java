159ebdfd197b94621a2913c260b1830e65d808a1
黄志鹏 <huangzhipeng02@58.com>
2022-09-28 16:29:37 +0800

    可运行

---
+++ SeedNetworkLib/src/main/java/com/seed/network/GitHubService.java
@@ -0,0 +1,9 @@
+package com.seed.network;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GitHubService {
+   /* @GET("users/{user}/repos")
+    Call<List<Repo>> listRepos(@Path("user") String user);*/
+}
